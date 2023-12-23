e17c657d2f0577aaebd18d6637a1cee66b18f606
varvara <kirylchyk@gmail.com>
2023-12-23 17:18:01 -0500

    Revert "change_11_added_Dashboard_component"
    This reverts commit 7eee28c149f0d7501605c7c5ea8862aa90843fef.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard'; // Ensure the path is correct
-import Main from './components/Main/Main'; // Ensure the path is correct
-import Header from './components/Header/Header'; // Ensure the path is correct
-import Footer from './components/Footer/Footer'; // Ensure the path is correct
-
-
-class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
-
-    static getDerivedStateFromError(error) {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
-    }
-
-    componentDidCatch(error, errorInfo) {
-        // You can also log the error to an error reporting service
-        console.error("Uncaught error:", error, errorInfo);
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return <h1>Something went wrong.</h1>;
-        }
-
-        return this.props.children;
-    }
-}
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+import ContentList from './components/ContentList/ContentList';
 
 
 function App() {
-    return (
-        <ErrorBoundary>
-            <Router>
-                <div>
-                    <Header />
-                    <Routes>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/" element={<Main />} />
-                    </Routes>
-                    <Footer />
-                </div>
-            </Router>
-        </ErrorBoundary>
-    );
+  return (
+      <div>
+        <Header />
+        <Main />
+
+          <div className="App">
+          </div>
+
+        <Footer />
+      </div>
+  );
 }
-
-
 
 export default App;
--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
 import styles from './Main.module.css';
 import ContentList from "../ContentList/ContentList";
 
@@ -7,13 +6,7 @@ function Main() {
     return (
         <main className={styles.mainContainer}>
             <h2 className={styles.mainTitle}>Main Content Area</h2>
-            <ContentList/>
-            <div>
-                <h1>Main Screen</h1>
-                <Link to="/dashboard">
-                    <button>Go to Dashboard</button>
-                </Link>
-            </div>
+            <ContentList />
         </main>
     );
 }
