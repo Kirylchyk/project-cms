53c132a84ad4da6c0466dd2991000518059d2739
varvara <kirylchyk@gmail.com>
2023-12-24 13:08:07 -0500

    change_12

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 function Dashboard() {
     const [cardCount, setCardCount] = useState(0);
     const [averagePrice, setAveragePrice] = useState(0);
+
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate('/');
+    };
 
     useEffect(() => {
         // Fetch card count
@@ -22,6 +30,7 @@ function Dashboard() {
         <div>
             <h1>Dashboard</h1>
             <div>
+                <button onClick={goBack}>Back to Main</button>
                 <h2>Card Count: {cardCount}</h2>
                 <h2>Average Price: {averagePrice.toFixed(2)}</h2>
             </div>
